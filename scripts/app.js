6ed83d8fa0b2d754ab1bc65a60b05cc25420f390
Tyron Clark <tyronclark97@gmail.com>
2022-07-06 18:24:39 -0400

    use of basic CSS / DOM manipulation

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#todo-form');
 const input = document.querySelector('#new-todo');
 const list = document.querySelector('#list');
+const submit = document.querySelector('#submit-todo')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -8,4 +9,8 @@ form.addEventListener('submit', (e) => {
     const newLI = document.createElement('LI');
     newLI.innerText = toDo;
     list.append(newLI);
+});
+
+form.addEventListener('submit', () => {
+    document.getElementById('new-todo').value='';
 });
