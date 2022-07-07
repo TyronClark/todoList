ac2eff68dcd1361b6aa2618f9c6d506624709de9
Tyron Clark <tyronclark97@gmail.com>
2022-07-07 04:55:57 -0400

    append a delete icon with list element
    trying to figure out how to display delete icon inline next to list element

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,22 @@ const input = document.querySelector('#new-todo');
 const list = document.querySelector('#list');
 const submit = document.querySelector('#submit-todo')
 
+//on submit take input and display as new list elements
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const toDo = input.value;
     const newLI = document.createElement('LI');
+    const image = document.createElement('img');
+    image.src = "/images/delete-icon.png"
     newLI.innerText = toDo;
     list.append(newLI);
+    list.appendChild(image);
+    //clear input value
+    document.getElementById('new-todo').value='';
+    
 });
 
+//on submit clear input value
 form.addEventListener('submit', () => {
-    document.getElementById('new-todo').value='';
+    
 });
